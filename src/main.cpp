c918f2f9e4c47a0494fd48af36914250864f97a1
kenrio <kenken7272721@gmail.com>
2025-11-21 01:53:29 +0900

    Draw a triangle with colors depending on its pixel position

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -44,16 +45,14 @@ int	main(void)
 
 	float	vertices[] =
 	{
-		0.5, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
 	};
 
 	unsigned int	indices[] =
 	{
-		0, 1, 3,
-		1, 2, 3,
+		0, 1, 2,
 	};
 
 	unsigned int	VBO;
@@ -72,16 +71,22 @@ int	main(void)
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
 
 	const char	*vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
-		"out vec4 vertexColor;\n"
+		"layout (location = 1) in vec3 aColor;\n"
+		"out vec3 ourColor;\n"
 		"void main()\n"
 		"{\n"
-		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+		"	gl_Position = vec4(aPos, 1.0);\n"
+		"	ourColor = aColor;\n"
 		"}\0";
 
 	unsigned int	vertexShader;
@@ -100,10 +105,10 @@ int	main(void)
 
 	const char	*fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
-		"in vec4 vertexColor;\n"
+		"in vec3 ourColor;\n"
 		"void main()\n"
 		"{\n"
-		"	FragColor = vertexColor;\n"
+		"	FragColor = vec4(ourColor, 1.0);\n"
 		"}\0";
 
 	unsigned int	fragmentShader;
@@ -145,9 +150,10 @@ int	main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
+
 		glBindVertexArray(VAO);
 		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
